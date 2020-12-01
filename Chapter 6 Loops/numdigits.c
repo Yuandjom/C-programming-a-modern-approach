174e8b1690236c23e2c2ffc99887c2681638d00e
Yuandjom <ziyangjohn@gmail.com>
2020-12-01 21:19:32 +0800

    done

--- Chapter 6 Loops/numdigits.c
+++ Chapter 6 Loops/numdigits.c
@@ -1,3 +1,4 @@
+// Basically do statement is just a whil estatement whose controlling expression is tested after each execution of the loop body 
 #include <stdio.h>
 
 int main (void)
@@ -7,10 +8,10 @@ int main (void)
     printf("Enter  a nonnegative integer: ");
     scanf("%d",&n);
 
-    do{
+    do{ // body loop execute first 
         n /= 10;
-        digits ++;
-    }while (n>0);
+        digits ++; 
+    }while (n>0); // then the controlling statement (last)
 
     printf("The number has %d digits(s).\n", digits);
 
